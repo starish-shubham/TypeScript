b34a07c59e7ce7efd0c6bdc2ac6d5314851cd4b6
Shubham_Gupta <shubhamk0108@gmail.com>
2025-05-04 10:59:07 +0530

    second commit

--- i.ts
+++ i.ts
@@ -1,7 +1,7 @@
 type Person = { name: string };
 const person: Person = { name: "Shubham" };
 
-console.log(person.name);
+console.log(person.name); 
 
 let myNum: string = "12";
 console.log(myNum);
