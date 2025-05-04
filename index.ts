e73d4c42efc4b45c8cdc1252fb494fb8a618740d
Shubham_Gupta <shubhamk0108@gmail.com>
2025-05-04 11:10:44 +0530

    any type

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 type Person = { name: string };
 const person: Person = { name: "Shubham" };
 
-console.log(person.name); 
+// console.log(person.name); 
 
 let myNum: string = "12";
-console.log(myNum);
+// console.log(myNum);
+
+let color:any = "red";
+console.log(color);
+color = color.toUpperCase();
+console.log(color);
+
+
+color=100;
+console.log(color);
+
+color=true;
+console.log(color);
+
+
+
