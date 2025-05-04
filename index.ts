ec9a86766104eb4940b940485ebdf5049f311375
Shubham_Gupta <shubhamk0108@gmail.com>
2025-05-04 13:56:00 +0530

    functions complete

--- index.ts
+++ index.ts
@@ -23,14 +23,41 @@ function addOne(n:number){
     return ++n;
 }
 
-console.log(addOne(2));
+// console.log(addOne(2));
 
 
 let addTwoNum = (x:number, y:number) => x+y;
-console.log(addTwoNum(1,4));
+// console.log(addTwoNum(1,4));
 
 let addTwoNum1: (x: number, y: number) => number = (x: number, y: number) => x + y;
-console.log(addTwoNum1(4, 6)); 
+// console.log(addTwoNum1(4, 6)); 
+
+const double = (x:number,y:number)=>x+y;
+// const res = double(3,3,0);
+// console.log(res);
+
+const greet=(person:string = "Shubham") => {return `Hello ${person}`};
+const resGreet:string = greet();
+// console.log(resGreet);
+// console.log(greet("abc"));
+
+const printMessage: (s: string) => void = (s: string) => console.log(s);
+// printMessage("ShubhamG");
+
+function printMessage1(s:string){
+    // console.log(s);
+}
+// printMessage1("m");
+
+function infiniteLoop(): string{
+    while (true) {
+        console.log("123");
+        
+    }
+}
+
+
+
 
 
 
