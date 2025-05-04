2acd81dde1a32beb010899ad0dc87b2b3847bac5
Shubham_Gupta <shubhamk0108@gmail.com>
2025-05-04 11:26:52 +0530

    playing with functions

--- index.ts
+++ index.ts
@@ -7,16 +7,30 @@ let myNum: string = "12";
 // console.log(myNum);
 
 let color:any = "red";
-console.log(color);
+// console.log(color);
 color = color.toUpperCase();
-console.log(color);
+// console.log(color);
 
 
 color=100;
-console.log(color); 
+// console.log(color); 
 
 color=true;
-console.log(color);
+// console.log(color);
+
+
+function addOne(n:number){
+    return ++n;
+}
+
+console.log(addOne(2));
+
+
+let addTwoNum = (x:number, y:number) => x+y;
+console.log(addTwoNum(1,4));
+
+let addTwoNum1: (x: number, y: number) => number = (x: number, y: number) => x + y;
+console.log(addTwoNum1(4, 6)); 
 
 
 
